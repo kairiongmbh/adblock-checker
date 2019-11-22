40e599effd8f26008da3229ee5e66c31fdff97e0
Sergei Ksenofontov <Sergey.Ksenofontov@kairion.de>
2019-11-22 15:03:16 +0100

    init

--- index.js
+++ index.js
@@ -1,84 +1,76 @@
 const got = require('got');
 const Tatler = require('tatler-client');
 const AdBlockClient = require('adblock-rs');
-const Promise = require('bluebird');
+const fs = require('fs');
+const path = require('path');
+const { StringStream } = require('scramjet');
 
+const ruleFileName = './rule_sources.txt';
 const {
-  tatlerName,
-  tatlerSecret,
-  sources,
-  domainsToCheck
-} = checkVars(process.env);
+  TATLER_NAME: tatlerName,
+  TATLER_SECRET: tatlerSecret
+} = process.env;
 
-const tatler = Tatler({
-  [tatlerName]: tatlerSecret
-}, tatlerName);
+let tatler = () => {};
+if (tatlerName && tatlerSecret) {
+  console.log('Tatler is switched off');
+  tatler = Tatler({
+    [tatlerName]: tatlerSecret
+  }, tatlerName);
+}
 
 // TODO: uncomment after new release
 // let resources = AdBlockClient.uBlockResources('uBlockOrigin/src/web_accessible_resources', 'uBlockOrigin/src/js/redirect-engine.js', 'uBlockOrigin/assets/resources/scriptlets.js');
 const placeToCheck = 'https://google.com'; // need a website example to make a check
 (async () => {
-  await Promise.map(sources.split('::'), async (source) => {
-    console.log(`Start ${source} processing`);
-    let ruleList;
-    try {
-      ruleList = (await got(source)).body;
-      if (!ruleList) {
-        return tatler(`Empty rule list. Source: ${source}`);
+  const ruleSources = await getRuleSources();
+  console.log('Ready to read from stdin!');
+  StringStream.from(process.stdin)
+    .setOptions({maxParallel: 4})
+    .lines()
+    .map(async (url) => {
+      let res = '';
+      for (let sourceName in ruleSources) {
+        const client = new AdBlockClient.Engine(ruleSources[sourceName].split('\n'), true);
+
+        const {
+          matched,
+          filter,
+          error
+        } = client.check(url, placeToCheck, 'host', true);
+
+        if (error) {
+          const errorText = `Error ${error} for url ${url} while checking ${sourceName}`;
+          res += errorText + '\n';
+          await tatler(errorText);
+        } else if (matched) {
+          const matchedText = `Url ${url} was matched in the list: ${sourceName} by the rule: ${filter}}`;
+          res += matchedText + '\n';
+          await tatler(matchedText);
+        } else {
+          res += `${url} wasn't found in ${sourceName} \n`;
+        }
       }
-    } catch (e) {
-      return tatler(`Error during rule list getting: ${JSON.stringify(e)}. Source: ${source}`);
-    }
-    console.log(`${source} was downloaded!`);
-
-    const client = new AdBlockClient.Engine(ruleList.split('\n'), true);
-    const checkResults = domainsToCheck
-      .split('::')
-      .map((domain) => client.check(`https://${domain}/`, placeToCheck, 'host', true));
-
-    const checkErrors = checkResults
-      .filter(({ error }) => error);
-    if (checkErrors.length) {
-      await tatler(`Error occures during the check: ${JSON.stringify(checkErrors)}. Source: ${source}`);
-    }
-
-    const checkMatches = checkResults
-      .filter(({ matched }) => matched);
-    console.log(`${source} check matches: `);
-    console.dir(checkMatches);
-    if (checkMatches.length) {
-      await tatler(`Found a record in adblock rule list! Source: ${source}, results: ${JSON.stringify(checkMatches)}`);
-    }
-  });
+      return res;
+    })
+    .pipe(process.stdout);
 })();
 
-function checkVars () {
-  const {
-    TATLER_NAME: tatlerName,
-    TATLER_SECRET: tatlerSecret,
-    SOURCES: sources,
-    DOMAINS_TO_CHECK: domainsToCheck
-  } = process.env;
-
-  console.log(tatlerSecret);
-
-  if (!tatlerName) {
-    throw new Error('Has no tatler pipeline name!');
-  }
-  if (!tatlerSecret) {
-    throw new Error('Has no tatler secret!');
-  }
-  if (!sources) {
-    throw new Error('Has no sources to download!');
-  }
-  if (!domainsToCheck) {
-    throw new Error('Has no domains to check!');
-  }
-
-  return {
-    tatlerName,
-    tatlerSecret,
-    sources,
-    domainsToCheck
-  };
+async function getRuleSources () {
+  const pathToTheRuleFile = path.join(__dirname, ruleFileName);
+  return StringStream
+    .from(fs.createReadStream(pathToTheRuleFile))
+    .setOptions({maxParallel: 4})
+    .lines()
+    .parse((source) => ({ source }))
+    .accumulate(async (acc, { source }) => {
+      console.log(`Started ${source} parsing`);
+      try {
+        const { body } = await got(source);
+        acc[source] = body;
+        console.log(`Source ${source} was successfully parsed!`);
+      } catch (e) {
+        await tatler(`There was an error during source parsing! Source: ${source}, error: ${JSON.stringify(e)}`);
+      }
+    }, {});
 }
